f7d930c5d611c4db550d036676b8a81e455bfd61
Gabriele Palmeri <36270614+gab-palmeri@users.noreply.github.com>
2022-12-02 19:02:57 +0100

    formalize object types for MyContext session data

--- src/bot/customTypes.ts
+++ src/bot/customTypes.ts
@@ -1,26 +1,31 @@
 import { Context, SessionFlavor } from "grammy";
 
 export type MyContext = Context & SessionFlavor<{
-    groups: {
-        groupName:string, 
-        groupId:number,
-        canCreate:number,
-        canDelete:number,
-        canRename:number,
-        canAddUsers:number,
-        canRemUsers:number,
-    }[], 
-    selectedGroup: {
-        groupName:string, 
-        groupId:number,
-        canCreate:number,
-        canDelete:number,
-        canRename:number,
-        canAddUsers:number,
-        canRemUsers:number,
-    },
+    groups: Groups, 
+    selectedGroup: SelectedGroup,
     lastUsedTags: LastUsedTags
 }>;
+
+type Groups = {
+    groupName:string, 
+    groupId:number,
+    canCreate:number,
+    canDelete:number,
+    canRename:number,
+    canAddUsers:number,
+    canRemUsers:number,
+}[]
+
+type SelectedGroup = {
+    groupName:string, 
+    groupId:number,
+    canCreate:number,
+    canDelete:number,
+    canRename:number,
+    canAddUsers:number,
+    canRemUsers:number,
+}
+
 
 export type LastUsedTags = {
     userId: string,

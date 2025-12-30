9873bb218583caaa2a222cf82223085ccbb321a9
gab-palmeri <gabriele.palmeri@proton.me>
2025-12-31 00:38:27 +0100

    update packages and code

--- src/index.ts
+++ src/index.ts
@@ -13,61 +13,33 @@ if (!process.env.BOT_TOKEN) {
 
 if(process.env.MODE == "polling") {
 
-	const startPolling = async () => {
-		const bot = await initializeBot();
-		const runner = run(
-			bot,
-			500,
-			{allowed_updates: ["message", "callback_query", "my_chat_member", "chat_member"]},
-			{retryInterval: 1000}
-		);
+	const bot = await initializeBot();
+	const runner = run(
+		bot,
+		500,
+		{allowed_updates: ["message", "callback_query", "my_chat_member", "chat_member"]},
+		{retryInterval: 1000}
+	);
 
-		const stopRunner = () => runner.isRunning() && runner.stop();
-		process.once("SIGINT", stopRunner);
-		process.once("SIGTERM", stopRunner);
+	const stopRunner = () => runner.isRunning() && runner.stop();
+	process.once("SIGINT", stopRunner);
+	process.once("SIGTERM", stopRunner);
 
-		console.log("Bot started in long polling mode");
-	};
-
-	startPolling().catch(() => {console.log("Error during start in polling mode");});
+	console.log("Bot started in long polling mode");
 
 }
 
 else if (process.env.MODE == "webhook") {
 
-	const startWebhook = async () => {
-		const bot = await initializeBot();
+	const bot = await initializeBot();
 
-		const app = express();
-		const port = process.env.PORT || 8080;
-		app.use(express.json());
-		app.use(webhookCallback(bot, 'express'));
-		app.listen(port, () => {
-			console.log(`Starting on webhook at port ${port}`);
-		});
-	};
-
-	startWebhook().catch(() => {console.log("Error during start in webhook mode");});
+	const app = express();
+	const port = process.env.PORT || 8080;
+	app.use(express.json());
+	app.use(webhookCallback(bot, 'express'));
+	app.listen(port, () => {
+		console.log(`Starting on webhook at port ${port}`);
+	});
 }
 
-
-/*
-public async start() {
-
-		const runner = run(
-			this.bot, 
-			500, 
-			{allowed_updates: ["message", "callback_query", "my_chat_member", "chat_member"]},
-			{retryInterval: 1000}
-		);
-
-		const stopRunner = () => runner.isRunning() && runner.stop();
-		process.once("SIGINT", stopRunner);
-		process.once("SIGTERM", stopRunner);
-	}
-
-	//bot getter
-	public getBot() {
-		return this.bot;
-	}
-*/
+await initializeBot();

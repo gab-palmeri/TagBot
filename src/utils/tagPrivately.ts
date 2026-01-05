66b7ab7e6538b685b1b9943815fd22e6799dee5b
gab-palmeri <gabriele.palmeri@proton.me>
2026-01-05 19:32:12 +0100

    feat: private language adoption on private tag

--- src/utils/tagPrivately.ts
+++ src/utils/tagPrivately.ts
@@ -1,17 +1,18 @@
 import { MyContext } from "utils/customTypes";
+import { UserDTO } from "db/user/user.dto";
 
-import { SubscriberDTO } from "db/subscriber/subscriber.dto";
+import i18n from "./i18n";
 
 //This function sends a private message to each user subscribed to the tag
-export async function tagPrivately(ctx: MyContext, tagName: string, groupName: string, subscribers: SubscriberDTO[], messageToReplyTo: number) {
+export async function tagPrivately(ctx: MyContext, tagName: string, groupName: string, subscribers: UserDTO[], messageToReplyTo: number) {
     const messageLink = "https://t.me/c/" + ctx.msg.chat.id.toString().slice(4) + "/" + messageToReplyTo;
     const notContacted = [];
 
-    // const toSendMessage = msgPrivateTag(tagName, groupName, messageLink);
-    const toSendMessage = ctx.t("tag.private-message", {tagName, groupName, messageLink});
+    
 
     for(const subscriber of subscribers) {
         try {
+            const toSendMessage = i18n.t(subscriber.lang, "tag.private-message", {tagName, groupName, messageLink});
             await ctx.api.sendMessage(subscriber.userId, toSendMessage, { parse_mode: "Markdown" });
         } catch(e) {
             notContacted.push((await ctx.getChatMember(parseInt(subscriber.userId))).user.first_name);

43d0882f8d7299ed90e1d09f87e42f9d0437f3c2
gab-palmeri <gabriele.palmeri@proton.me>
2026-01-05 22:04:30 +0100

    feat: group name change listener

--- src/handlers/listeners/message.ts
+++ src/handlers/listeners/message.ts
@@ -1,9 +1,15 @@
 import { MyContext } from "utils/customTypes";
 import UserRepository from "db/user/user.repository";
+import GroupRepository from "db/group/group.repository";
 
 export async function messageHandler(ctx: MyContext) {
 
     const userRepository = new UserRepository();
+    
+    if(ctx.msg.new_chat_title) {
+        const groupRepository = new GroupRepository();
+        await groupRepository.update(ctx.chatId.toString(), {groupName: ctx.msg.new_chat_title});
+    }
 
     // Invoke user retrieval
     const user = await userRepository.getUser(ctx.from.id.toString());

1324b07416ab3923a8c2e37f0aff872cb3b489bf
gab-palmeri <gabriele.palmeri@proton.me>
2026-01-05 21:49:07 +0100

    feat: user creation on private start as well

--- src/handlers/commands/start.ts
+++ src/handlers/commands/start.ts
@@ -1,6 +1,20 @@
+import UserRepository from "db/user/user.repository";
 import { MyContext } from "utils/customTypes";
 
 export async function startCommandHandler(ctx: MyContext) {
+
+    if(ctx.chat.type == "private") {
+        const userRepository = new UserRepository();
+        const userId = ctx.chatId.toString();
+        const user = await userRepository.getUser(userId);
+
+        if(user === null) {
+            await userRepository.saveUser(userId, ctx.chat.username || "");
+        }
+        else if(!user.hasBotStarted) {
+            await userRepository.setBotStarted(userId, true);
+        }
+    }
 
     return ctx.reply(ctx.t("start"), { parse_mode: "Markdown", link_preview_options: { is_disabled: true }});
    

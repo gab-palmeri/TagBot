a9b4053b016f25b63e005afda473ed638bdffa1a
gab-palmeri <m.gabriele.palmeri@gmail.com>
2026-01-05 13:30:48 +0100

    remove webhook support

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import express from 'express';
 import { webhookCallback } from 'grammy';
 
 import initializeBot from "./initializeBot";
@@ -8,36 +7,18 @@ if (!process.env.BOT_TOKEN) {
 	throw new Error("BOT_TOKEN is not defined in environment variables");
 }
 
+const bot = await initializeBot();
+const runner = run(
+	bot,
+	500,
+	{allowed_updates: ["message", "callback_query", "my_chat_member", "chat_member"], drop_pending_updates: true},
+	{retryInterval: 1000}
+);
 
-if(process.env.MODE == "polling") {
+const stopRunner = () => runner.isRunning() && runner.stop();
+process.once("SIGINT", stopRunner);
+process.once("SIGTERM", stopRunner);
 
-	const bot = await initializeBot();
-	const runner = run(
-		bot,
-		500,
-		{allowed_updates: ["message", "callback_query", "my_chat_member", "chat_member"], drop_pending_updates: true},
-		{retryInterval: 1000}
-	);
-
-	const stopRunner = () => runner.isRunning() && runner.stop();
-	process.once("SIGINT", stopRunner);
-	process.once("SIGTERM", stopRunner);
-
-	console.log("Bot started in long polling mode");
-
-}
-
-else if (process.env.MODE == "webhook") {
-
-	const bot = await initializeBot();
-
-	const app = express();
-	const port = process.env.PORT || 8080;
-	app.use(express.json());
-	app.use(webhookCallback(bot, 'express'));
-	app.listen(port, () => {
-		console.log(`Starting on webhook at port ${port}`);
-	});
-}
+console.log("Bot started in long polling mode");
 
 await initializeBot();

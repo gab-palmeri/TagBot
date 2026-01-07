42f7420f587a01c59ee13a70559bea2156219bbe
gab-palmeri <gabriele.palmeri@proton.me>
2026-01-07 11:09:52 +0100

    remove unused LastUsed type

--- src/utils/customTypes.ts
+++ src/utils/customTypes.ts
@@ -6,14 +6,8 @@ import { I18nFlavor } from "@grammyjs/i18n";
 export type MyContext = Context & I18nFlavor & SessionFlavor<{
     groups: Groups, 
     selectedGroup: GroupDTO | null,
-    lastUsedTags: LastUsedTags
 }>;
 
 export type Groups = GroupDTO[]
-
-export type LastUsedTags = {
-    userId: string,
-    timestamps: number[],
-}[]
 
 export type TranslateFn = (key: string, params?: Record<string, string|number>) => string;
